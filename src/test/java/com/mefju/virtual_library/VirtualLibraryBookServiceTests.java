fad709f655063af3426ea44d44a46506f7e45c4e
Mateusz <82820649+Mefju001@users.noreply.github.com>
2024-01-16 16:14:11 +0100

    create test for Book Service

--- src/test/java/com/mefju/virtual_library/VirtualLibraryBookServiceTests.java
+++ src/test/java/com/mefju/virtual_library/VirtualLibraryBookServiceTests.java
@@ -1,21 +1,33 @@
 package com.mefju.virtual_library;
 
 import com.mefju.virtual_library.Entity.Book;
+import com.mefju.virtual_library.Repository.BookRepository;
 import com.mefju.virtual_library.Service.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class VirtualLibraryBookServiceTests {
     // Testy dla BookService
     @Autowired
     private BookService bookService;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @MockBean
+    private BookService bookServiceMock;
+
     @Test
     void TestFindAllBooks()
     {
@@ -40,5 +52,31 @@ class VirtualLibraryBookServiceTests {
         assertNotNull(result);
         assertFalse(result.isEmpty());
         assertEquals("Wampirze Cesarstwo", result.get(0).getName());
+    }
+    @Test
+    void TestSave()
+    {
+        // Przygotowanie danych testowych
+        Book newBook = new Book();
+        newBook.setName("Wampirze Cesarstwo");
+
+        bookServiceMock.save(newBook);
+        verify(bookServiceMock).save(newBook); // Zweryfikuj mocka
+    }
+    @Test
+    void TestSaveAndFindAndDelete()
+    {
+        Book book = new Book();
+        book.setName("Test");
+        bookRepository.save(book);
+
+        Optional<Book>savedbook = bookRepository.findById(book.getId());
+        assertTrue(savedbook.isPresent());
+        assertEquals("Test",savedbook.get().getName());
+
+        bookRepository.deleteById(book.getId());
+        Optional<Book> deletedBook = bookRepository.findById(book.getId());
+        assertFalse(deletedBook.isPresent());
+
     }
 }
